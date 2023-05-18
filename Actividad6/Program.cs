a18e53240136c3e8695b529115e1b32ec9365e8d
Erik Tapia <20203tn152@utez.edu.mx>
2023-05-18 12:37:41 -0600

    mensaje

---
+++ Actividad6/Program.cs
@@ -0,0 +1,105 @@
+﻿class Program
+
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese la antigüedad del empleado en meses:");
+
+        if (!int.TryParse(Console.ReadLine(), out int antiguedad))
+        {
+            Console.WriteLine("Error: ¡Ingrese solo números!");
+            return;
+        }
+
+        Console.WriteLine("Ingrese la cantidad de hijos del empleado:");
+
+        if (!int.TryParse(Console.ReadLine(), out int cantidadHijos))
+        {
+            Console.WriteLine("Error: ¡Ingrese solo números!");
+            return;
+        }
+
+        string departamento = CalcularDepartamento(antiguedad);
+        double bonoAntiguedad = CalcularBonoAntiguedad(departamento, antiguedad);
+        double bonoHijos = CalcularBonoHijos(cantidadHijos);
+
+        double bonoTotal = bonoAntiguedad + bonoHijos;
+
+        Console.WriteLine("El departamento del empleado es: " + departamento);
+        Console.WriteLine("El bono por antigüedad es: " + bonoAntiguedad);
+        Console.WriteLine("El bono por hijos es: " + bonoHijos);
+        Console.WriteLine("El bono total es: " + bonoTotal);
+    }
+
+    static string CalcularDepartamento(int antiguedad)
+    {
+        if (antiguedad < 24 || (antiguedad >= 24 && antiguedad <= 35))
+        {
+            return "a";
+        }
+        else if (antiguedad < 36 || (antiguedad >= 36 && antiguedad <= 59))
+        {
+            return "b";
+        }
+        else
+        {
+            return "c";
+        }
+    }
+
+    static double CalcularBonoAntiguedad(string departamento, int antiguedad)
+    {
+        double salarioMinimo = 180;
+
+        if (departamento == "a")
+        {
+            if (antiguedad < 24)
+            {
+                return 5 * salarioMinimo;
+            }
+            else
+            {
+                return 8 * salarioMinimo;
+            }
+        }
+        else if (departamento == "b")
+        {
+            if (antiguedad < 36)
+            {
+                return 9 * salarioMinimo;
+            }
+            else
+            {
+                return 12 * salarioMinimo;
+            }
+        }
+        else if (departamento == "c")
+        {
+            if (antiguedad < 60)
+            {
+                return 14 * salarioMinimo;
+            }
+            else
+            {
+                return 17 * salarioMinimo;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Departamento inválido.");
+            return 0;
+        }
+    }
+
+    static double CalcularBonoHijos(int cantidadHijos)
+    {
+        double bonoPorHijo = 150;
+
+        return bonoPorHijo * cantidadHijos;
+    }
+
+
+}
+
+
+
