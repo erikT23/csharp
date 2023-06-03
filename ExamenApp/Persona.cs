a964646172afb7b26f113fc0f09cb61b01307d35
Erik <20203tn152@utez.edu.mx>
2023-06-02 20:15:03 -0600

    recupera

---
+++ ExamenApp/Persona.cs
@@ -0,0 +1,181 @@
+﻿using System;
+
+public class Persona
+{
+    private string? nombre;
+
+    private int edad;
+
+    private float estatura;
+
+    private char? sexo;
+
+    private bool estadoCivil;
+
+    public Persona(string? nombre, int edad, float estatura, char? sexo, bool estadoCivil)
+    {
+        this.Nombre = nombre;
+        this.Edad = edad;
+        this.Estatura = estatura;
+        this.Sexo = sexo;
+        this.EstadoCivil = estadoCivil;
+    }
+
+    public string? Nombre { get => nombre; set => nombre = value; }
+    public int Edad { get => edad; set => edad = value; }
+    public float Estatura { get => estatura; set => estatura = value; }
+    public char? Sexo { get => sexo; set => sexo = value; }
+    public bool EstadoCivil { get => estadoCivil; set => estadoCivil = value; }
+
+
+    public string Obtener()
+    {
+        Console.WriteLine("Nombre de usuario: " + (nombre ?? "N/A"));
+        Console.WriteLine("Edad de usuario: " + edad);
+        Console.WriteLine("Estatura Usuario: " + estatura);
+        Console.WriteLine("Sexo Usuario: " + sexo);
+
+        if (!estadoCivil)
+        {
+            Console.WriteLine("Soltero");
+        }
+        else
+        {
+            Console.WriteLine("Casado");
+        }
+
+        return nombre ?? string.Empty;
+    }
+
+
+
+    public string? Modificar()
+    {
+        Console.WriteLine("Que dato desea modificar?");
+        Console.WriteLine("1.-Nombre \n 2.- Edad \n 3.-Estatura \n 4.-Sexo \n 5.-Estado Civil \n 6.-Regresar al menú anterior ");
+        string? eleccionTmp = Console.ReadLine();
+        int eleccion = Convert.ToInt32(eleccionTmp);
+
+        switch (eleccion)
+        {
+            case 1:
+
+                Console.WriteLine("Esta seguro que desea hacer el cambio? \n 1- Si \n 2 - No");
+                string? confirmarTmp = Console.ReadLine();
+                int confirmar = Convert.ToInt32(confirmarTmp);
+
+
+                if (confirmar == 1)
+                {
+                    Console.WriteLine("Ingrese su nuevo nombre");
+                    string? NuevoNombre = Console.ReadLine();
+                    nombre = NuevoNombre;
+                }
+                else
+                {
+                    break;
+                }
+
+                break;
+
+            case 2:
+                Console.WriteLine("Esta seguro que desea hacer el cambio? \n 1- Si \n 2 - No");
+                string? confirmarTmp2 = Console.ReadLine();
+                int confirmar2 = Convert.ToInt32(confirmarTmp2);
+
+                if (confirmar2 == 1)
+                {
+                    Console.WriteLine("Ingrese su nueva edad");
+                    string? NuevaEdadTmp = Console.ReadLine();
+                    int NuevaEdad = Convert.ToInt32(NuevaEdadTmp);
+                    edad = NuevaEdad;
+                }
+                else
+                {
+                    break;
+                }
+
+                break;
+
+            case 3:
+                Console.WriteLine("Esta seguro que desea hacer el cambio? \n 1- Si \n 2 - No");
+                string? confirmarTmp3 = Console.ReadLine();
+                int confirmar3 = Convert.ToInt32(confirmarTmp3);
+
+                if (confirmar3 == 1)
+                {
+                    Console.WriteLine("Ingrese su nueva estatura");
+                    string? NuevaEstaturaTmp = Console.ReadLine();
+                    float NuevaEstatura = Convert.ToInt32(NuevaEstaturaTmp);
+
+                    estatura = NuevaEstatura;
+                }
+                else
+                {
+                    break;
+                }
+
+                break;
+
+            case 4:
+                Console.WriteLine("Esta seguro que desea hacer el cambio? \n 1- Si \n 2 - No");
+                string? confirmarTmp4 = Console.ReadLine();
+                int confirmar4 = Convert.ToInt32(confirmarTmp4);
+
+                if (confirmar4 == 1)
+                {
+                    Console.WriteLine("Ingrese su nuevo sexo");
+                    string? NuevoSexotmp = Console.ReadLine();
+                    char NuevoSexo;
+                    if (!char.TryParse(NuevoSexotmp, out NuevoSexo))
+                    {
+                        Console.WriteLine("Sexo inválido. Debe ser 'F' o 'M'.");
+                    }
+                    else
+                    {
+                        sexo = NuevoSexo;
+                    }
+
+                }
+                else
+                {
+                    break;
+                }
+
+
+                break;
+
+            case 5:
+                Console.WriteLine("Esta seguro que desea hacer el cambio? \n 1- Si \n 2 - No");
+                string? confirmarTmp5 = Console.ReadLine();
+                int confirmar5 = Convert.ToInt32(confirmarTmp5);
+                if (confirmar5 == 1)
+                {
+                    Console.WriteLine("Ingrese su nuevo estado civil \n 1 casado \n 0 soltero");
+                    string? NuevoEstadoCivilTmp = Console.ReadLine();
+                    int NuevoEstadoCivil = Convert.ToInt32(NuevoEstadoCivilTmp);
+                    if (NuevoEstadoCivil == 1)
+                    {
+                        estadoCivil = true;
+                    }
+                    else
+                    {
+                        estadoCivil = false;
+                    }
+
+                }
+                else
+                {
+                    break;
+                }
+
+                break;
+
+            case 6:
+                break;
+
+        }
+
+        return nombre;
+    }
+}
---
+++ ExamenApp/TestPersona.cs
@@ -0,0 +1,120 @@
+using System;
+
+public class TestPersona
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Bienvenido, ingresa un nombre");
+        string? Nombre = Console.ReadLine();
+
+        Console.WriteLine("Ingresa la edad");
+        if (!int.TryParse(Console.ReadLine(), out int Edad))
+        {
+            Console.WriteLine("Edad inválida. Debe ser un número entero.");
+            return;
+        }
+
+        Console.WriteLine("Ingresa la estatura con formato decimal: ejemplo 1.70");
+        if (!float.TryParse(Console.ReadLine(), out float Estatura))
+        {
+            Console.WriteLine("Estatura inválida. Debe ser un número decimal.");
+            return;
+        }
+
+        Console.WriteLine("Ingresa el sexo: F o M");
+        if (!char.TryParse(Console.ReadLine(), out char Sexo))
+        {
+            Console.WriteLine("Sexo inválido. Debe ser 'F' o 'M'.");
+            return;
+        }
+
+        Console.WriteLine("Ingresa el estado civil: S = Soltero, C = Casado");
+        if (!char.TryParse(Console.ReadLine(), out char Estado))
+        {
+            Console.WriteLine("Estado civil inválido. Debe ser 'S' o 'C'.");
+            return;
+        }
+
+        Estado = char.ToLower(Estado);
+        bool EstadoCivil;
+        switch (Estado)
+        {
+            case 'c':
+                EstadoCivil = true;
+                break;
+            case 's':
+                EstadoCivil = false;
+                break;
+            default:
+                Console.WriteLine("Valor incorrecto, ingrese 'S' para soltero o 'C' para casado");
+                return;
+        }
+
+        Persona objPersona = new Persona(Nombre, Edad, Estatura, Sexo, EstadoCivil);
+        Console.WriteLine("Registro exitoso");
+
+        int opcion = 0;
+
+        while (opcion != 3)
+        {
+            Console.WriteLine("1. Mostrar los datos");
+            Console.WriteLine("2. Cambiar los datos");
+            Console.WriteLine("3. Salir");
+
+            if (!int.TryParse(Console.ReadLine(), out opcion))
+            {
+                Console.WriteLine("Opción incorrecta. Por favor, ingresa un número válido.");
+                continue;
+            }
+
+            switch (opcion)
+            {
+                case 1:
+                    Console.WriteLine("¿Estás seguro de que deseas mostrar los datos? (S/N)");
+                    string? confirmacionMostrar = Console.ReadLine();
+                    if (confirmacionMostrar?.ToUpper() == "S")
+                    {
+                        Console.WriteLine("Datos de usuario");
+                        Console.WriteLine(objPersona.Obtener());
+                    }
+                    else
+                    {
+                        Console.WriteLine("Operación cancelada.");
+                    }
+                    break;
+
+                case 2:
+                    Console.WriteLine("¿Estás seguro de que deseas cambiar los datos? (S/N)");
+                    string? confirmacionCambiar = Console.ReadLine();
+                    if (confirmacionCambiar?.ToUpper() == "S")
+                    {
+                        objPersona.Modificar();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Operación cancelada.");
+                    }
+                    break;
+
+                case 3:
+                    Console.WriteLine("¿Estás seguro de que deseas salir? (S/N)");
+                    string? confirmacionSalir = Console.ReadLine();
+                    if (confirmacionSalir?.ToUpper() == "S")
+                    {
+                        Console.WriteLine("Adiós.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Operación cancelada.");
+                        opcion = 0;
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Opción incorrecta. Por favor, selecciona una opción válida.");
+                    opcion = 0;
+                    break;
+            }
+        }
+    }
+}
